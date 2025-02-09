8031eed2f07066ae659285836d40379cd367873a
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-08 21:09:07 -0700

    Configure and test cookies and sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+
+  # Method to test cookies and sessions
+  def hello_world
+    session[:count] = (session[:count] || 0) + 1
+    render json: { count: session[:count] }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # route to test your configuration
+  get "/hello", to: "application#hello_world"
 end
